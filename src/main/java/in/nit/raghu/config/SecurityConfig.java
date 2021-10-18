549f5c74951a64bbd64325cdf0e1aecff5119d14
gadekarshital61 <gadekrshital61@gmail.com>
2021-10-17 23:56:06 -1200

    spring srcurity example

---
+++ src/main/java/in/nit/raghu/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package in.nit.raghu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+          auth.inMemoryAuthentication().withUser("sam").password("{noop}sam").authorities("ADMIN");
+          auth.inMemoryAuthentication().withUser("syed").password("{noop}syed").authorities("EMPLOYEE");
+          auth.inMemoryAuthentication().withUser("jai").password("{noop}jai").authorities("STUDENT");
+	}
+	
+	protected void configure(HttpSecurity http)throws Exception{
+		//URL access type
+		http.authorizeRequests()
+		.antMatchers("/home").permitAll()
+		.antMatchers("/welcome").authenticated()
+		.antMatchers("/admin").hasAuthority("ADMIN")
+		.antMatchers("/emp").hasAuthority("EMPLOYEE")
+		.antMatchers("/emp").hasAuthority("STUDENT")
+		
+		//Login details
+		.and()
+		.formLogin()
+        .defaultSuccessUrl("/welcome",true)		
+        
+		//logout details
+		.and()
+		.logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		
+		//exception handling
+		.and()
+		.exceptionHandling()
+		.accessDeniedPage("/denied")
+		
+		;
+	}
+
+	
+}
